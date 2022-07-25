2409e8bcfd848fc0edf535da98f96c038c96950f
Administrator <Administrator@DESKTOP-N5VR3R3>
2022-07-25 14:55:53 +0800

    base version

---
+++ TeamFpp20220725/src/main/java/com/example/demo/TeamFpp20220725Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFpp20220725Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamFpp20220725Application.class, args);
+	}
+
+}
